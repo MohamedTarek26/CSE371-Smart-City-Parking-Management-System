ad1c609e75e57b31ddf6040d5f627ca81ea3ef25
asserelzeki6 <asserelzeki@gmail.com>
2024-12-26 07:32:35 +0200

    deleting ineeded files

--- project/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createRouter } from './router'
-import './style.css'
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(createRouter())
-app.mount('#app')
